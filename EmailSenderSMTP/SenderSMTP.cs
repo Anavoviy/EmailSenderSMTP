2e3064e475dcf80fd6e0e385dc7df43b9393d399
Anavoviy <vovan200512345@mail.ru>
2024-04-01 21:43:58 +1000

    Version 1.0.1 - Add Async Methods - Add Configure Name Author MailMessage in ctor SenderSMTP

--- EmailSenderSMTP/SenderSMTP.cs
+++ EmailSenderSMTP/SenderSMTP.cs
@@ -5,9 +5,7 @@ namespace EmailSenderSMTP
 {
     public class SenderSMTP
     {
-        
-
-
+        private string NameAuthor { get;  } 
         private string Host { get; }
         private int Port { get; }
         private string EmailFrom { get; }
@@ -16,13 +14,14 @@ namespace EmailSenderSMTP
         private SmtpClient SmtpClient { get; }
 
 
-        public SenderSMTP(MailService mailService, string emailFrom, string pass, int emailsPerMinutes = 10)
+        public SenderSMTP(MailService mailService, string emailFrom, string pass, string nameAuthor = "", int emailsPerMinutes = 10)
         {
             Host = SetHost(mailService);
             Port = SetPort(mailService);
             EmailFrom = emailFrom;
             Pass = pass;
             EmailPerMinutes = emailsPerMinutes;
+            NameAuthor = nameAuthor;
 
 
             // Настройка SmtpClient
@@ -32,55 +31,78 @@ namespace EmailSenderSMTP
 
             this.SmtpClient = smtp;
         }
-
         
-        public MailMessage CreateMailMessageBodyIsHTML(string name, string emailTo, string subject = "", string body = "")
+
+        public MailMessage CreateMailMessageBodyIsHTML(string emailTo, string name = null, string subject = "", string bodyHTML = "")
         {
             var from = new MailAddress(this.EmailFrom, name);
             var to = new MailAddress(emailTo);
             var mail = new MailMessage(from, to);
 
             mail.Subject = subject;
-            mail.Body = body;
+            mail.Body = bodyHTML;
             mail.IsBodyHtml = true;
 
             return mail;
         }
-        public MailMessage CreateMailMessageBodyIsText(string name, string emailTo, string subject = "", string body = "")
+        public MailMessage CreateMailMessageBodyIsText( string emailTo, string name = null, string subject = "", string bodyText = "")
         {
-            var from = new MailAddress(this.EmailFrom, name);
+            var from = new MailAddress(this.EmailFrom);
+            if (name == null)
+                from = new MailAddress(this.EmailFrom, this.NameAuthor);
+            else
+                from = new MailAddress(this.EmailFrom, name);
+
             var to = new MailAddress(emailTo);
             var mail = new MailMessage(from, to);
 
             mail.Subject = subject;
-            mail.Body = body;
+            mail.Body = bodyText;
 
             return mail;
         }
 
-        public async void SendMail(MailMessage mail)
+        public void SendMail(MailMessage mail)
         {
             this.SmtpClient.Send(mail);
         }
+        public void SendMail(string emailTo, string name = null, string subject = "", string bodyText = "")
+        {
+            MailMessage mail = this.CreateMailMessageBodyIsText(emailTo, name, subject, bodyText);
+            this.SmtpClient.Send(mail);
+        }
+
+        public async Task SendMailAsync(MailMessage mail)
+        {
+            await this.SmtpClient.SendMailAsync(mail);
+        }
+        public async Task SendMailAsync(string emailTo, string name = null, string subject = "", string bodyText = "")
+        {
+            MailMessage mail = this.CreateMailMessageBodyIsText(emailTo, name, subject, bodyText);
+            await this.SmtpClient.SendMailAsync(mail);
+        }
+
         public void SendMailToAddresses(MailMessage message, IEnumerable<string> addresses)
         {
             try
             {
-                long counter = 0;
+                List<MailMessage> messages = new List<MailMessage>();
+
                 if (message.IsBodyHtml)
                     foreach (string address in addresses)
-                    {
-                        if (counter % EmailPerMinutes == 0)
-                            Thread.Sleep(60000);
+                        messages.Add(this.CreateMailMessageBodyIsHTML(address, message.From.DisplayName, message?.Subject, message?.Body));
+                else
+                    foreach (string address in addresses)
+                        messages.Add(this.CreateMailMessageBodyIsText(address, message.From.DisplayName, message?.Subject, message?.Body));
 
-                        MailMessage mailMessage = this.CreateMailMessageBodyIsHTML(message.From.DisplayName, address, message?.Subject, message?.Body);
-                        this.SendMail(mailMessage);
-
-                        counter++;
-                    }
+                foreach (MailMessage oneMessage in messages)
+                {
+                    Thread.Sleep(60000 / this.EmailPerMinutes);
+                    this.SendMail(oneMessage);
+                }
 
             } 
-            catch (Exception ex) { throw ex; }
+            catch (Exception ex) { Logger.Log(ex.Message); }
         }
         public void SendMailsToAddresses(IEnumerable<MailMessage> mails)
         {
@@ -88,13 +110,49 @@ namespace EmailSenderSMTP
             {
                 if (mails == null) throw new ArgumentNullException();
 
-                MailMessage[] mailMessages = mails.ToArray();
-
-
-                for (int i = 0; i < mails.Count(); i++)
-                    this.SendMail(mailMessages[i]);
+                foreach (MailMessage mail in mails)
+                {
+                    Thread.Sleep(60000 / this.EmailPerMinutes);
+                    this.SendMail(mail);
+                }
             }
-            catch (Exception ex) { throw ex; }
+            catch (Exception ex) { Logger.Log(ex.Message); }
+        }
+
+        public async Task SendMailToAddressesAsync(MailMessage message, IEnumerable<string> addresses)
+        {
+            try
+            {
+                List<MailMessage> messages = new List<MailMessage>();
+
+                if (message.IsBodyHtml)
+                    foreach (string address in addresses)
+                        messages.Add(this.CreateMailMessageBodyIsHTML(address, message.From.DisplayName, message?.Subject, message?.Body));
+                else
+                    foreach(string address in addresses)
+                        messages.Add(this.CreateMailMessageBodyIsText(address, message.From.DisplayName, message?.Subject, message?.Body));
+
+                foreach (MailMessage oneMessage in messages)
+                {
+                    Thread.Sleep(60000 / this.EmailPerMinutes);
+                    await this.SendMailAsync(oneMessage);
+                }
+            }
+            catch (Exception ex) { Logger.Log(ex.Message); }
+        }
+        public async Task SendMailsToAddressesAsync(IEnumerable<MailMessage> mails)
+        {
+            try
+            {
+                if (mails == null) throw new ArgumentNullException();
+
+                foreach (MailMessage oneMessage in mails)
+                {
+                    Thread.Sleep(60000 / this.EmailPerMinutes);
+                    await this.SendMailAsync(oneMessage);
+                }
+            }
+            catch (Exception ex) { Logger.Log(ex.Message); }
         }
 
         private string SetHost(MailService mailService)
